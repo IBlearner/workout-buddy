b20fad6c8a2588aa4dd5bdfbee006f3e2341eb50
kienvi <kienvi96@hotmail.com>
2023-09-05 15:54:10 +1000

    Adding basic stopwatch feature.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [timer, setTimer] = useState(0);
+	const [isTimerRunning, setIsTimerRunning] = useState(false);
+
+	const updateTimer = () => {
+		setTimer(timer + 1);
+
+		setInterval(() => {
+			// updateTimer();
+			console.log(timer);
+		}, 1000);
+	};
+
+	return (
+		<View style={styles.container}>
+			<Text>Shoulder press</Text>
+			<Text>Timer: {timer}</Text>
+			<Button title="Begin set" color={"green"} onPress={updateTimer}></Button>
+			<Button title="Finish set" color={"orange"}></Button>
+			<Button title="Choose another exercise"></Button>
+			<TextInput style={styles.input} placeholder="fds"></TextInput>
+
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	input: {
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		padding: 10
+	}
 });
